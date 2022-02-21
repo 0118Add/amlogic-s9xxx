227b3f04b2b2f56e2765ba738df596a33589d14d
0118Add <60157610+0118Add@users.noreply.github.com>
2022-02-21 08:59:41 +0800

    Update diy-part1_n1_lede.sh

--- diy-part1_n1_lede.sh
+++ diy-part1_n1_lede.sh
@@ -19,11 +19,20 @@
 echo 'src-git amlogic https://github.com/ophub/luci-app-amlogic' >>feeds.conf.default
 
 # echo '删除重复插件'
-rm -rf ./package/lean/luci-app-netdata
-rm -rf ./package/lean/luci-app-jd-dailybonus
-rm -rf ./package/lean/luci-lib-docker
-rm -rf ./package/lean/luci-app-cpufreq
-rm -rf ./package/lean/luci-app-dockerman
+rm -rf ./package/luci/applications/luci-app-netdata
+rm -rf ./package/luci/applications/luci-app-jd-dailybonus
+rm -rf ./package/luci/applications/luci-lib-docker
+rm -rf ./package/luci/applications/luci-app-cpufreq
+rm -rf ./package/luci/applications/luci-app-dockerman
+
+#git clone https://github.com/jerrykuku/luci-app-vssr.git package/luci-app-vssr
+#git clone https://github.com/jerrykuku/lua-maxminddb.git package/lua-maxminddb
+git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
+git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman
+git clone -b revert-271-18.06_bing https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon-18.06
+git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
+git clone https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+git clone https://github.com/immortalwrt/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
 
 # 替换index.htm文件 n1
 wget -O ./package/lean/autocore/files/arm/index.htm https://raw.githubusercontent.com/0118Add/Amlogic-s9xxx/main/diy/n1_lede/index/index_N1.htm
